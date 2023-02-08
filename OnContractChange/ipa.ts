a388e47409b9e1e1c759d17d8f9066b1a89309b9
Rodolfo Viti <rodolfo.viti@pagopa.it>
2023-02-08 11:12:07 +0100

    fix suggestions from PR

--- OnContractChange/ipa.ts
+++ OnContractChange/ipa.ts
@@ -6,13 +6,22 @@ import { Transform } from "stream";
 import { parse } from "csv-parse";
 
 export type IpaCode = string;
-export type FiscalCode = string | undefined;
+export type FiscalCode = string;
+/**
+ * A map representing organizations retrieved from IPA, with the IPA code as the key and the corresponding tax code as the value.
+ */
 export type IpaOpenData = ReadonlyMap<IpaCode, FiscalCode>;
 
+/**
+ * Read data from a {@link stream} and transform its content into an {@link IpaOpenData} instance
+ *
+ * @param stream a stream reader of IPA Open Data (CSV formatted)
+ * @returns an {@link IpaOpenData} instance
+ */
 export const readIpaData = async (
   stream: NodeJS.ReadableStream
 ): Promise<IpaOpenData> => {
-  const ipaCode2FiscalCode = new Map<string, string | undefined>();
+  const ipaCode2FiscalCode = new Map<string, string>();
   await pipeline(
     stream,
     parse({ delimiter: ",", from_line: 2 }),
