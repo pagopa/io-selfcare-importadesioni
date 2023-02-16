8a71d813adc125af51e5f704fb0e972cbaf66064
Emanuele De Cupis <ema.decu@gmail.com>
2023-02-16 11:35:01 +0100

    refactor: type refinement

--- models/types.ts
+++ models/types.ts
@@ -1,17 +1,41 @@
+import { NonNegativeNumber } from "@pagopa/ts-commons/lib/numbers";
+import {
+  EmailString,
+  FiscalCode,
+  NonEmptyString
+} from "@pagopa/ts-commons/lib/strings";
 import * as t from "io-ts";
 
 export type IpaCode = t.TypeOf<typeof IpaCode>;
 export const IpaCode = t.string;
 
+export type MembershipStatus = t.TypeOf<typeof MembershipStatus>;
+export const MembershipStatus = t.union([
+  t.literal("Initial"),
+  t.literal("Processed"),
+  t.literal("Discarded")
+]);
+
+const Institution = t.union([
+  t.type({
+    fiscalCode: NonEmptyString,
+    ipaCode: IpaCode,
+    mainInstitution: t.literal(true)
+  }),
+  t.intersection([
+    t.type({ ipaCode: IpaCode, mainInstitution: t.literal(false) }),
+    t.partial({ fiscalCode: NonEmptyString })
+  ])
+]);
+
 export type IMembership = t.TypeOf<typeof IMembership>;
 export const IMembership = t.intersection([
+  Institution,
   t.type({
-    id: t.string,
-    ipaCode: IpaCode,
-    mainInstitution: t.boolean,
+    id: t.string, // same value as ipaCode
     status: t.string
   }),
-  t.partial({ fiscalCode: t.string })
+  t.partial({ note: t.string })
 ]);
 
 export type IAttachment = t.TypeOf<typeof IAttachment>;
@@ -38,19 +62,21 @@ export const TipoDelegato = t.union([
   t.literal("Altro")
 ]);
 
-export type Delegate = t.TypeOf<typeof Delegate>;
-export const Delegate = t.type({
-  email: t.string,
-  fiscalCode: t.string,
-  id: t.string,
-  role: TipoDelegato
+export type PecDelegate = t.TypeOf<typeof PecDelegate>;
+export const PecDelegate = t.interface({
+  CODICEFISCALE: FiscalCode,
+  EMAIL: EmailString,
+  IDALLEGATO: NonNegativeNumber,
+  NOMINATIVO: t.union([NonEmptyString, t.null]),
+  TIPODELEGATO: TipoDelegato,
+  id: NonEmptyString
 });
 
 export type IContractWithDelegates = t.TypeOf<typeof IContractWithDelegates>;
 export const IContractWithDelegates = t.intersection([
   IContract,
   t.type({
-    delegates: t.readonlyArray(Delegate)
+    delegates: t.readonlyArray(PecDelegate)
   })
 ]);
 
