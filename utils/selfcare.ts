da911900ed1f0da7371c4d3183ebcaa16b9a7c78
Emanuele De Cupis <ema.decu@gmail.com>
2023-03-02 11:15:10 +0100

    fix selfcare basepath

--- utils/selfcare.ts
+++ utils/selfcare.ts
@@ -11,10 +11,21 @@ type Prettify<T> = {
 
 export type SelfCareClient = Prettify<Client<"apiKeyHeader">>;
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const f = async (
+  ...[a, b]: Parameters<typeof nodeFetch>
+): ReturnType<typeof nodeFetch> => {
+  console.log("--->", a, b);
+  const r = await nodeFetch(a, b);
+  console.log("+++++", r.status);
+  return r;
+};
+
 export const createClient = (baseUrl: string, apiKey: string): SelfCareClient =>
   createClientBase({
+    basePath: "",
     baseUrl,
-    fetchApi: (nodeFetch as unknown) as typeof fetch,
+    fetchApi: (f as unknown) as typeof fetch,
     // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
     withDefaults: op => params =>
       op({
