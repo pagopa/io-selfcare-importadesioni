9ea042199dfdb4fd57a46459a91944a057da91a0
Rodolfo Viti <rodolfo.viti@pagopa.it>
2023-07-11 12:08:42 +0200

    use common app insight

--- infra/resources/monitoring.tf
+++ infra/resources/monitoring.tf
@@ -1,9 +1,17 @@
 # Application insights
-resource "azurerm_application_insights" "application_insights" {
-  name                = format("%s-%s-appinsights", local.project, var.application_basename)
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  application_type    = "Node.JS"
 
-  tags = var.tags
+variable "application_insights_name" {
+  type        = string
+  description = "Application Insight instance name"
+  default     = null
+}
+variable "application_insights_rg_name" {
+  type        = string
+  description = "Application Insight resource group name"
+  default     = null
+}
+
+data "azurerm_application_insights" "application_insights" {
+  name                = var.application_insights_name
+  resource_group_name = var.application_insights_rg_name
 }
