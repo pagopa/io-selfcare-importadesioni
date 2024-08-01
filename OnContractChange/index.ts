55d51a174476467a97ab7df2af24f324d336a814
AleDore <alessio85.dore@gmail.com>
2024-08-01 15:09:15 +0200

    add comment

--- OnContractChange/index.ts
+++ OnContractChange/index.ts
@@ -8,6 +8,7 @@ import { createIpaDataReader } from "./ipa";
 const config = getConfigOrThrow();
 
 const blobService = createBlobService(
+  // TODO here insert the new primary storage connection string
   config.INTERNAL_STORAGE_CONNECTION_STRING
 );
 
