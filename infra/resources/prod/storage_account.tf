4d040311bf5465c843f3388bbaae7f22b12ccd2d
ccuffari <75679158+ccuffari@users.noreply.github.com>
2024-11-11 17:53:08 +0100

    [CES-472] Added new storage account iopimportadesionist for ITN migration with latests suggestions

--- infra/resources/prod/storage_account.tf
+++ infra/resources/prod/storage_account.tf
@@ -88,8 +88,9 @@ module "azure_storage_account" {
   resource_group_name = azurerm_resource_group.itn.name
   tier                = "l"
 
-  subnet_pep_id                        = data.azurerm_subnet.subnet_pep_itn.id
-  private_dns_zone_resource_group_name = "${local.prefix}-${local.env_short}-rg-common"
+
+  subnet_pep_id                        = module.common_values.pep_subnets.itn.id
+  private_dns_zone_resource_group_name = module.common_values.resource_groups.weu.common
 
   subservices_enabled = {
     blob  = true
