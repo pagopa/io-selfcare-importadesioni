3b4fd383e9bc37b0ad629ca3b3651d9956e9ada8
Stefano D'Aversa <stefano.daversa@pagopa.it>
2025-02-17 12:53:38 +0100

    add permission for team to resource group (#59)

--- infra/resources/prod/resource_group.tf
+++ infra/resources/prod/resource_group.tf
@@ -11,3 +11,25 @@ resource "azurerm_resource_group" "itn" {
 
   tags = local.tags
 }
+
+resource "azurerm_role_assignment" "devs_group_rg_es" {
+  scope                = azurerm_resource_group.rg.id
+  role_definition_name = "Owner"
+  principal_id         = data.azuread_group.svc_devs.object_id
+  description          = "Allow ES Dev group to manage resource group"
+}
+
+resource "azurerm_role_assignment" "devs_group_rg_es_itn" {
+  scope                = azurerm_resource_group.itn.id
+  role_definition_name = "Owner"
+  principal_id         = data.azuread_group.svc_devs.object_id
+  description          = "Allow ES Dev group to manage resource group"
+}
+
+#
+# External dependency
+#
+
+data "azuread_group" "svc_devs" {
+  display_name = "${local.project}-adgroup-svc-developers"
+}
